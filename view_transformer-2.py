9e95461600a94910679abc03c4bfa642ec771646
Carlos E. Jimenez <cjsaltlake@gmail.com>
2024-01-30 01:23:48 -0500

    Add view_transformer-2

--- view_transformer-2.py
+++ view_transformer-2.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 from collections import defaultdict, deque
+from collections import defaultdict, deque
 
 import cv2
 import numpy as np
@@ -27,9 +28,11 @@ class ViewTransformer:
     def __init__(self, source: np.ndarray, target: np.ndarray) -> None:
         source = source.astype(np.float32)
         target = target.astype(np.float32)
+        target = target.astype(np.float32)
         self.m = cv2.getPerspectiveTransform(source, target)
 
     def transform_points(self, points: np.ndarray) -> np.ndarray:
         reshaped_points = points.reshape(-1, 1, 2).astype(np.float32)
+        transformed_points = cv2.perspectiveTransform(reshaped_points, self.m)
         transformed_points = cv2.perspectiveTransform(reshaped_points, self.m)
         return transformed_points.reshape(-1, 2)
